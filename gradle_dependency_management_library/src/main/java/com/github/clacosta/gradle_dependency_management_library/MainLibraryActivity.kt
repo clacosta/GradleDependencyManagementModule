0b1a435b43b6400cf0a63e78af7e72e7c12b19fa
Claudio Costa <Claudio.RobertoNogueiraCosta-ext@ab-inbev.com>
2020-12-28 17:01:37 -0300

    Adding module

--- gradle_dependency_management_library/src/main/java/com/github/clacosta/gradle_dependency_management_library/MainLibraryActivity.kt
+++ gradle_dependency_management_library/src/main/java/com/github/clacosta/gradle_dependency_management_library/MainLibraryActivity.kt
@@ -1,9 +1,9 @@
-package com.github.clacosta.gradledependencymanagementmodule
+package com.github.clacosta.gradle_dependency_management_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_library)
